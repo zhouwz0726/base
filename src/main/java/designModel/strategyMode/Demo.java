a4d2c9dd93dccd198c8540c23890a6b3fef09d0c
周文钟 <532917026@qq.com>
2018-09-28 19:25:14 +0800

    策略模式

--- src/main/java/designModel/strategyMode/Demo.java
+++ src/main/java/designModel/strategyMode/Demo.java
@@ -3,8 +3,12 @@ package designModel.strategyMode;
 public class Demo {
 
     public static void main(String[] args) {
+
         Context context = new Context(new OperationAdd());
         System.out.println("10 + 5 = " + context.executeStrategy(10,5));
+
+        Context context1 = new Context(new OperationSubstract());
+        System.out.println("10 - 5 = " + context1.executeStrategy(10,5));
 
     }
 }
