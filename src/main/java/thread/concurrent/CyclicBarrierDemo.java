029260eb663d5a04430d1637569b4d39871912ae
zhouwz <532917026@qq.com>
2018-11-13 22:37:22 +0800

    cyclicBarrier类和semaphore类的解释和demo的书写

---
+++ src/main/java/thread/concurrent/CyclicBarrierDemo.java
@@ -0,0 +1,49 @@
+package thread.concurrent;
+
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * cyclicBarrier:栅栏,是一个同步机制，使得一系列算法实现同步
+ * cyclicBarrier:让一组线程到达一个屏障（同步点）时阻塞，直到最后一个线程到达后，才会释放这个屏障;
+ * cyclicBarrier默认的构造方法里面的参数，表示当前栅栏的线程数量，每个线程调用await方法告诉cyclicBarrier我已经到达屏障，然后当线程被阻塞
+ * 例子：当下面线程1和线程2都调用awit方法后，cyclicBarrier的线程数量设置为2，达到了设置的数量，然后就会执行输出语句
+ * 当然static CyclicBarrier c = new CyclicBarrier(2, new A());参数new A代表了，当达到屏障数量后，可以优先执行new A
+ * @author zhouwz
+ * */
+public class CyclicBarrierDemo {
+
+    static CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+
+    public static void main(String[] args) {
+
+        //线程1
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    cyclicBarrier.await();
+                    System.out.println("当前运行线程为：" + Thread.currentThread().getName());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //线程2
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    cyclicBarrier.await();
+                    System.out.println("当前运行线程为：" + Thread.currentThread().getName());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
---
+++ src/main/java/thread/concurrent/semaphoreDemo.java
@@ -0,0 +1,48 @@
+package thread.concurrent;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+/**
+ * semaphore:计数信号量，主要2个方法，acquire()，获取一个许可;release()，吧当前的许可返回给信号量
+ * 例子:6个人需要进入停车场，然后停车就3个位置，一开始进去3个后，后面必须等前面的人出来（释放）才可以进去
+ * semaphore，没有办法保证线程能够公平地可从信号量中获得许可。
+ * 也就是说，无法担保掉第一个调用 acquire() 的线程会是第一个获得一个许可的线程。如果第一个线程在等待一个许可时发生阻塞，而第二个线程前来索要一个许可的时候刚好有一个许可被释放出来，那么它就可能会在第一个线程之前获得许可。
+ * 如果你想要强制公平，Semaphore 类有一个具有一个布尔类型的参数的构造子，通过这个参数以告知 Semaphore 是否要强制公平。强制公平会影响到并发性能，所以除非你确实需要它否则不要启用它。
+ * @author zhouwz
+ * */
+public class semaphoreDemo extends Thread {
+
+    private static final Semaphore semaphore = new Semaphore(3,true);
+
+
+        private String name ;
+
+        public semaphoreDemo (String name){
+            this.name = name;
+        }
+
+        public void run(){
+            try {
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + "大家好：我是" + name);
+                Thread.sleep(1000);
+                System.out.println(name+"要准备释放许可证了，当前时间为："+System.currentTimeMillis());
+                System.out.println("当前可使用的许可数为："+semaphore.availablePermits());
+                semaphore.release();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
+    public static void main(String[] args) {
+        String nameArr[] = {"张三","李四","王五","周六","戚七","陶八",};
+        for (String n:nameArr) {
+            Thread thread  = new semaphoreDemo(n);
+            thread.start();
+        }
+    }
+}
