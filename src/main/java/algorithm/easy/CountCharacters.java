ba7a1dcdd560306a11ae9d2f1b76f3e09e54a3fa
周文钟 <wenzhong.zhou@tongdun.net>
2020-03-24 16:54:24 +0800

    更改格式

--- src/main/java/algorithm/easy/CountCharacters.java
+++ src/main/java/algorithm/easy/CountCharacters.java
@@ -3,26 +3,25 @@ package algorithm.easy;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 示例1：
- * 输入：words = ["cat","bt","hat","tree"], chars = "atach"
- * 输出：6
- * 解释：可以形成字符串 "cat" 和 "hat"，所以答案是 3 + 3 = 6。
- *
- * 示例2：
- * 输入：words = ["hello","world","leetcode"], chars = "welldonehoneyr"
- * 输出：10
- * 解释：可以形成字符串 "hello" 和 "world"，所以答案是 5 + 5 = 10。
- * */
 public class CountCharacters {
 
     public static void main(String[] args) {
         String[] words = {"cat", "bt", "hat", "tree"};
         String chars = "atach";
         int result = countCharacters(words, chars);
-        System.out.println(result);
     }
 
+    /**
+     * 示例1：
+     * 输入：words = ["cat","bt","hat","tree"], chars = "atach"
+     * 输出：6
+     * 解释：可以形成字符串 "cat" 和 "hat"，所以答案是 3 + 3 = 6。
+     *
+     * 示例2：
+     * 输入：words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+     * 输出：10
+     * 解释：可以形成字符串 "hello" 和 "world"，所以答案是 5 + 5 = 10。
+     * */
     public static int countCharacters(String[] words, String chars) {
         int result = 0;
         Map<Character, Integer> charsMap = new HashMap<>();
@@ -44,6 +43,7 @@ public class CountCharacters {
                 result = result + word.length();
             }
         }
+        System.out.println(result);
         return result;
     }
 }
