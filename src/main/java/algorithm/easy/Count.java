8b7bbf65161a47932da5b5e25223c3b1f1071770
周文钟 <wenzhong.zhou@tongdun.net>
2020-03-25 10:47:28 +0800

    更改格式

--- src/main/java/algorithm/easy/Count.java
+++ src/main/java/algorithm/easy/Count.java
@@ -3,7 +3,7 @@ package algorithm.easy;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CountCharacters {
+public class Count {
 
     public static void main(String[] args) {
         String[] words = {"cat", "bt", "hat", "tree"};
@@ -21,6 +21,8 @@ public class CountCharacters {
      * 输入：words = ["hello","world","leetcode"], chars = "welldonehoneyr"
      * 输出：10
      * 解释：可以形成字符串 "hello" 和 "world"，所以答案是 5 + 5 = 10。
+     *
+     * 思路：只要计算words中单个元素他的各个字母出现的次数小于chars中各个字母出现的次数，理论上我们就认为能形成
      * */
     public static int countCharacters(String[] words, String chars) {
         int result = 0;
